685219de12a79146f2aec256334bddc9559ae6ca
Abdullah Alblooshi <aakhair12@gmail.com>
2022-08-17 17:15:11 +0100

    not finished, play with it later

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+
+
+
+5.times do
+  Flat.create(name: Faker::Restaurant.name ,  address: Faker::Address.country)
+end
+
+puts "done"
